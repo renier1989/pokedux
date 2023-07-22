7e7c7a7b8a901dbb3a809beeb6b1038b22ac2373
Renier Vargas <renier1989@gmail.com>
2023-07-21 20:12:06 -0500

    Class 8  - implementing redux , react-redux to the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import getPokemo from "./Api";
 import "./App.css";
 import PokemonList from "./Components/PokemonList";
 import Searcher from "./Components/Searcher";
+import { setPokemons as setPokemonsActions } from "./actions";
 import {useEffect, useState } from 'react';
+import { connect } from "react-redux";
 
-function App() {
+function App({pokemons, setPokemons}) {
+console.log("🚀 ~ file: App.jsx:10 ~ App ~ pokemons:", pokemons)
 
-  const [pokemons ,setPokemons ]  = useState([]);
+  // const [pokemons ,setPokemons ]  = useState([]);
 
   useEffect(()=>{
     // crate an Async function to fetch Pokemon from the api
@@ -27,4 +30,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state => ({
+  pokemons: state.pokemons,
+})) 
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { pokemonsReducer } from './actions/reducers/pokemons.js'
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore} from 'redux'
+
+const store = createStore(pokemonsReducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
